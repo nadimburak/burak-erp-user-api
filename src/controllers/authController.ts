f35c40f5aa5049a9c2582678ac5a4b064bac0b31
Ubaid Ali <arc49254@gmail.com>
2025-05-07 12:28:32 +0530

    Fixed

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -5,9 +5,11 @@ import User, { IUser } from "../models/User";
 import { generateToken, verifyToken } from "../utils/jwt";
 import { AuthRequest, tokenBlacklist } from "../interfaces/Auth";
 
+const asyncHandler = require("express-async-handler");
+
 const modelTitle = "User";
 
-export const signIn = async (req: Request, res: Response) => {
+export const signIn = asyncHandler(async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
@@ -27,9 +29,9 @@ export const signIn = async (req: Request, res: Response) => {
   } catch (error: any) {
     res.status(500).json({ message: `Error ${modelTitle}.`, error });
   }
-};
+});
 
-export const signUp = async (req: Request, res: Response) => {
+export const signUp = asyncHandler(async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
     const existingUser = await User.findOne({ email });
@@ -48,9 +50,9 @@ export const signUp = async (req: Request, res: Response) => {
   } catch (error) {
     res.status(500).json({ message: `Error ${modelTitle}.`, error });
   }
-};
+});
 
-export const signOut = async (req: AuthRequest, res: Response) => {
+export const signOut = asyncHandler(async (req: AuthRequest, res: Response) => {
   try {
     const token = req.token;
     // Add the token to the blacklist
@@ -65,156 +67,170 @@ export const signOut = async (req: AuthRequest, res: Response) => {
   } catch (error) {
     res.status(500).json({ message: `Error ${modelTitle}.`, error });
   }
-};
+});
 
-export const getProfile = async (req: AuthRequest, res: Response) => {
-  try {
-    // Fetch the user's data from the database
-    const user = await User.findById(req.user._id).select("-password"); // Exclude the password
-    if (!user) {
-      res.status(404).json({ message: "User not found." });
+export const getProfile = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    try {
+      // Fetch the user's data from the database
+      const user = await User.findById(req.user._id).select("-password"); // Exclude the password
+      if (!user) {
+        res.status(404).json({ message: "User not found." });
+      }
+
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: `Error ${modelTitle}.`, error });
     }
-
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: `Error ${modelTitle}.`, error });
   }
-};
+);
 
-export const updateProfile = async (req: AuthRequest, res: Response) => {
-  try {
-    const { name, mobile } = req.body;
+export const updateProfile = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    try {
+      const { name, mobile } = req.body;
 
-    // Find the company by ID
-    const users = await User.findById(req.user._id);
-    if (!users) {
-      res.status(404).json({ message: "User not found." });
+      // Find the company by ID
+      const users = await User.findById(req.user._id);
+      if (!users) {
+        res.status(404).json({ message: "User not found." });
+      }
+
+      // Update fields with the new data (if provided)
+      const updatedProfile = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+          ...(name && { name }),
+          ...(mobile && { mobile }),
+        },
+        { new: true } // Return the updated document
+      );
+
+      res.status(200).json({
+        message: "Profile updated successfully.",
+        updatedProfile,
+      });
+    } catch (error: any) {
+      res.status(500).json({ message: `Error ${modelTitle}.`, error });
     }
-
-    // Update fields with the new data (if provided)
-    const updatedProfile = await User.findByIdAndUpdate(
-      req.user._id,
-      {
-        ...(name && { name }),
-        ...(mobile && { mobile }),
-      },
-      { new: true } // Return the updated document
-    );
-
-    res.status(200).json({
-      message: "Profile updated successfully.",
-      updatedProfile,
-    });
-  } catch (error: any) {
-    res.status(500).json({ message: `Error ${modelTitle}.`, error });
   }
-};
+);
 
-export const forgotPassword = async (req: Request, res: Response) => {
-  try {
-    const { email } = req.body;
+export const forgotPassword = asyncHandler(
+  async (req: Request, res: Response) => {
+    try {
+      const { email } = req.body;
 
-    // Find user by email
-    const user = await User.findOne({ email });
-    if (!user) {
-      res.status(404).json({ message: "User not found." });
-      return;
+      // Find user by email
+      const user = await User.findOne({ email });
+      if (!user) {
+        res.status(404).json({ message: "User not found." });
+        return;
+      }
+
+      const token = await generateToken(user._id as string);
+
+      // Reset URL
+      const resetUrl = `${process.env.CLIENT_URL}/auth/reset-password?token=${token}`;
+
+      // Send Email
+      const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST, // Replace with your SMTP server host
+        port: Number(process.env.EMAIL_PORT), // Common port for SMTP (587 for TLS, 465 for SSL)
+        secure: false, // True for 465 (SSL), false for other ports
+        auth: {
+          user: process.env.EMAIL_HOST_USER,
+          pass: process.env.EMAIL_HOST_PASSWORD,
+        },
+      });
+
+      await transporter.sendMail({
+        from: process.env.DEFAULT_FROM_EMAIL,
+        to: email,
+        subject: "Password Reset Request",
+        text: `Click this link to reset your password: ${resetUrl}`,
+      });
+
+      res.status(200).json({
+        message: "Password reset email sent successfully.",
+      });
+    } catch (error: any) {
+      console.log("error", error);
+      res.status(500).json({ message: `Error ${modelTitle}.`, error });
     }
-
-    const token = await generateToken(user._id as string);
-
-    // Reset URL
-    const resetUrl = `${process.env.CLIENT_URL}/auth/reset-password?token=${token}`;
-
-    // Send Email
-    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST, // Replace with your SMTP server host
-      port: Number(process.env.EMAIL_PORT), // Common port for SMTP (587 for TLS, 465 for SSL)
-      secure: false, // True for 465 (SSL), false for other ports
-      auth: {
-        user: process.env.EMAIL_HOST_USER,
-        pass: process.env.EMAIL_HOST_PASSWORD,
-      },
-    });
-
-    await transporter.sendMail({
-      from: process.env.DEFAULT_FROM_EMAIL,
-      to: email,
-      subject: "Password Reset Request",
-      text: `Click this link to reset your password: ${resetUrl}`,
-    });
-
-    res.status(200).json({
-      message: "Password reset email sent successfully.",
-    });
-  } catch (error: any) {
-    console.log("error", error);
-    res.status(500).json({ message: `Error ${modelTitle}.`, error });
   }
-};
+);
 
-export const verifyResetToken = async (req: Request, res: Response) => {
-  try {
-    const { token, password } = req.body;
+export const verifyResetToken = asyncHandler(
+  async (req: Request, res: Response) => {
+    try {
+      const { token, password } = req.body;
 
-    if (!token) {
-      res.status(400).json({ message: "Token is missing." });
-      return;
+      if (!token) {
+        res.status(400).json({ message: "Token is missing." });
+        return;
+      }
+      const decodedToken = await verifyToken(token as string);
+
+      // Check if the token is valid
+      if (!decodedToken) {
+        res.status(400).json({ message: "Invalid token." });
+        return;
+      }
+
+      // Find user by ID
+      const user = await User.findById(decodedToken.userId);
+      if (!user) {
+        res.status(404).json({ message: "User not found." });
+        return;
+      }
+
+      const updatedData: Partial<IUser> = {};
+      if (password) {
+        const hashedPassword = await bcrypt.hash(String(password), 10);
+        updatedData.password = hashedPassword; // Update password only if provided
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(
+        decodedToken.userId,
+        updatedData,
+        { new: true, runValidators: true } // Ensures the updated data adheres to the schema
+      ).select("-password"); // Exclude the password field from the result
+
+      if (!updatedUser) {
+        res.status(404).json({ message: `${modelTitle} not found.` });
+      }
+
+      res
+        .status(200)
+        .json({ message: "Password has been reset successfully." });
+    } catch (error) {
+      res.status(500).json({ message: `Error ${modelTitle}.`, error });
     }
-    const decodedToken = await verifyToken(token as string);
-
-    // Check if the token is valid
-    if (!decodedToken) {
-      res.status(400).json({ message: "Invalid token." });
-      return;
-    }
-
-    // Find user by ID
-    const user = await User.findById(decodedToken.userId);
-    if (!user) {
-      res.status(404).json({ message: "User not found." });
-      return;
-    }
-
-    const updatedData: Partial<IUser> = {};
-    if (password) {
-      const hashedPassword = await bcrypt.hash(String(password), 10);
-      updatedData.password = hashedPassword; // Update password only if provided
-    }
-
-    const updatedUser = await User.findByIdAndUpdate(
-      decodedToken.userId,
-      updatedData,
-      { new: true, runValidators: true } // Ensures the updated data adheres to the schema
-    ).select("-password"); // Exclude the password field from the result
-
-    if (!updatedUser) {
-      res.status(404).json({ message: `${modelTitle} not found.` });
-    }
-
-    res.status(200).json({ message: "Password has been reset successfully." });
-  } catch (error) {
-    res.status(500).json({ message: `Error ${modelTitle}.`, error });
   }
-};
+);
 
-export const resetPassword = async (req: AuthRequest, res: Response) => {
-  try {
-    const { newPassword } = req.body;
+export const resetPassword = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    try {
+      const { newPassword } = req.body;
 
-    // Find user by ID
-    const user = await User.findById(req.user._id);
-    if (!user) {
-      res.status(404).json({ message: "User not found." });
-      return;
+      // Find user by ID
+      const user = await User.findById(req.user._id);
+      if (!user) {
+        res.status(404).json({ message: "User not found." });
+        return;
+      }
+
+      // Update the password
+      user.password = newPassword; // Ensure password hashing in your model's `pre-save` middleware
+      await user.save();
+
+      res
+        .status(200)
+        .json({ message: "Password has been reset successfully." });
+    } catch (error) {
+      res.status(500).json({ message: `Error ${modelTitle}.`, error });
     }
-
-    // Update the password
-    user.password = newPassword; // Ensure password hashing in your model's `pre-save` middleware
-    await user.save();
-
-    res.status(200).json({ message: "Password has been reset successfully." });
-  } catch (error) {
-    res.status(500).json({ message: `Error ${modelTitle}.`, error });
   }
-};
+);
