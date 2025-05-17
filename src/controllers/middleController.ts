a2277af1b4eeb853cd77d518835b03fe560fd550
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2025-05-17 23:45:27 +0530

    add user

--- src/controllers/middleController.ts
+++ src/controllers/middleController.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
 import { AuthRequest } from "../interfaces/Auth";
 import { verifyToken } from "../utils/jwt";
+import User from "../models/User";
 
 const asyncHandler = require("express-async-handler");
 
@@ -14,15 +15,21 @@ export const verifyTokenHandle = asyncHandler(async (req: AuthRequest, res: Resp
       res.status(400).json({ message: "Token is missing." });
       return;
     }
-    const decodedToken = await verifyToken(token as string);
+    const verified = await verifyToken(token as string);
 
     // Check if the token is valid
-    if (!decodedToken) {
+    if (!verified) {
       res.status(400).json({ message: "Invalid token." });
       return;
     }
 
-    res.status(200).json({ message: "Token Verified." });
+    let user = null;
+    if (typeof verified !== "string" && "userId" in verified) {
+      const userId = verified.userId;
+      user = await User.findById(userId);
+    }
+
+    res.status(200).json({ message: "Token Verified.", user });
   } catch (error) {
     console.error(error)
     res.status(500).json({ message: `Error ${modelTitle}.`, error });
