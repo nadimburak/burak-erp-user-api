3eeffe4ac950535f22ee0b73cfe352b817f84f68
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2025-05-19 11:43:40 +0530

    add middleware

--- src/controllers/middleController.ts
+++ src/controllers/middleController.ts
@@ -2,6 +2,8 @@ import { Response } from "express";
 import { AuthRequest } from "../interfaces/Auth";
 import { verifyToken } from "../utils/jwt";
 import User from "../models/User";
+import { authorizeRole } from "../middlewares/auth.middleware";
+import Role from "../models/Role";
 
 const asyncHandler = require("express-async-handler");
 
@@ -30,6 +32,35 @@ export const verifyTokenHandle = asyncHandler(async (req: AuthRequest, res: Resp
     }
 
     res.status(200).json({ message: "Token Verified.", user });
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: `Error ${modelTitle}.`, error });
+  }
+});
+
+export const authorizePermissionHandle = asyncHandler(async (req: AuthRequest, res: Response) => {
+  try {
+    const { permission } = req.body;
+
+    if (!permission) {
+      res.status(400).json({ message: "Permission is missing." });
+      return;
+    }
+
+    const role = await Role.findById(req.user.role).populate<{
+      permissions: any[];
+    }>("permissions");
+
+    const hasPermission = role?.permissions.some(
+      (p: any) => p.name.toLowerCase() === permission.toLowerCase()
+    );
+
+    if (!hasPermission) {
+      return res.status(403).send({ message: "Forbidden" });
+    }
+
+    res.status(200).send({ message: "Authorized!" });
+
   } catch (error) {
     console.error(error)
     res.status(500).json({ message: `Error ${modelTitle}.`, error });
