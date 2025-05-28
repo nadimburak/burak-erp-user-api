63defd3d16848bb9d0d2230d88ffbaaa1663247f
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2025-05-28 15:52:35 +0530

    fix user type

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import {
   createUser,
-  getALLUsers,
   deleteUser,
   getUser,
   getUsers,
@@ -13,8 +12,6 @@ import { authorizePermission } from "../middlewares/auth.middleware";
 const router = express.Router();
 
 router.get("/users", authorizePermission("Can view user"), getUsers);
-router.get("/allUsers", authorizePermission("Can view user"), getALLUsers);
-
 router.get("/users/:id", authorizePermission("Can view user"), getUser);
 router.post("/users", authorizePermission("Can create user"), createUser);
 router.put("/users/:id", authorizePermission("Can edit user"), updateUser);
