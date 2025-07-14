e806d3acbd427446c42a64be0fc6ecea2188797e
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2025-07-14 14:55:14 +0530

    rm location

--- src/routes/location/cities.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllCity } from "../../controllers/location/cityLocationsController";
-
-const router = express.Router();
-
-router.get("/cities", getAllCity);
-
-export default router;
--- src/routes/location/countries.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllCountry } from "../../controllers/location/countryLocationsController";
-
-const router = express.Router();
-
-router.get("/countries", getAllCountry);
-
-export default router;
