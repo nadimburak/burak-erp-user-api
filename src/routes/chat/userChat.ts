321d886ea7182f886c5aad4f04eb12be86f108a4
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2025-08-11 11:43:58 +0530

    dd

--- src/routes/chat/userChat.ts
+++ src/routes/chat/userChat.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { createUserChat, getUserChat } from "../../controllers/chat/userChatController";
-import { authenticate } from "../../middlewares/auth";
+import { authenticate } from "../../middlewares/auth.middleware";
 
 const router = express.Router();
 
