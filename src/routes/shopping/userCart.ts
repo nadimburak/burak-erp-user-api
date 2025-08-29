25c17da65690194d8fe6192ec79e8f338c4db8af
sahil sheikh <mohdsahilsheikh5@gmail.com>
2025-08-29 16:48:13 +0530

    fixed

--- src/routes/shopping/userCart.ts
+++ src/routes/shopping/userCart.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import { authenticate } from "../../middlewares/auth.middleware";
-import { clearUserCart, createUserCart, deleteUserCart, getUserCarts } from "../../controllers/shopping/userCartController";
+import {
+  clearUserCart,
+  createUserCart,
+  deleteUserCart,
+  getUserCarts,
+  updateUserCart,
+} from "../../controllers/shopping/userCartController";
 
 const router = express.Router();
 
@@ -8,6 +14,7 @@ router.use(authenticate);
 
 router.get("/user-carts", getUserCarts);
 router.post("/user-carts", createUserCart);
+router.patch("/user-carts/:id", updateUserCart);
 router.delete("/user-carts/:id", deleteUserCart);
 router.delete("/user-carts", clearUserCart);
 
